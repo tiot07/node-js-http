259991234ca0bc6790d52ace937567df39f51216
tiot07 <tiot7155@gmail.com>
2019-01-01 02:22:45 +0900

    js記入

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 'use strict';
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{
+        'Content-Type':'text/plain; charset=utf-8'
+    });
+    const port = 8000;
+    server.listen(port,()=>{
+        console.log('Listening on '+port);
+    });
+})
